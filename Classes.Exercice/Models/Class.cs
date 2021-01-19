dc4ae2c8d67bb352c3461a9f43ad922652690972
sshimabuku <shimasstrafez@gmail.com>
2021-01-19 17:57:02 -0500

    Implemented classes

---
+++ Classes.Exercice/Models/Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Exercice.Models
+{
+    public class Class
+    {
+        private string name;
+        private string teacher;
+
+        private Subject subject;
+        private List<Student> students;
+
+        public Class(string name, string teacher, Subject subject, List<Student> students)
+        {
+            this.name = name;
+            this.teacher = teacher;
+            this.subject = subject;
+            this.students = students;
+        }
+
+        public string Teacher { get; }
+        public string Subject {
+            get 
+            { 
+                return subject.Name; 
+            }
+        }
+        public List<Student> Students { 
+            get 
+            { 
+                return students.OrderBy(s => s.FullName).ToList(); 
+            }
+        }
+
+        #region Methods
+
+        public void AddStudent(Student student)
+        {
+            this.students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            this.students.Remove(student);
+        }
+
+        #endregion
+    }
+}
---
+++ Classes.Exercice/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Exercice.Models
+{
+    public class Student
+    {
+        private string lastName;
+        private string firstName;
+        private DateTime birthday;
+
+        public string FullName { 
+            get
+            {
+                return firstName + ", " + lastName;
+            } 
+        }
+
+        public int Age
+        {
+            get
+            {
+                // Save today's date.
+                DateTime today = DateTime.Today;
+
+                // Calculate the age.
+                int age = today.Year - birthday.Year;
+
+                // Go back to the year in which the person was born in case of a leap year
+                if (birthday.Date > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+    }
+}
